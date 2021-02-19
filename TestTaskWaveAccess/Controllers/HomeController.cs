f444e7a09bc47f7bda4bc8a4a2a15ef6d0fd2687
Роман Бачурин <romanbachurin@gmail.com>
2021-02-19 12:59:23 +0500

    Learned Redirects in ASP.NET

--- TestTaskWaveAccess/Controllers/HomeController.cs
+++ TestTaskWaveAccess/Controllers/HomeController.cs
@@ -11,30 +11,16 @@ namespace TestTaskWaveAccess.Controllers
 {
 	public class HomeController : Controller
 	{
-		List<User> users;
-
 		private readonly ILogger<HomeController> _logger;
 
 		public HomeController(ILogger<HomeController> logger)
 		{
-			users = new List<User>
-			{
-				new User {Id = 1, Age = 20, Bio = "Hiii", IsBlocked = false, Name = "Roman", RegistrationDate = DateTime.Now },
-				new User {Id = 2, Age = 30, Bio = "I'm coder", IsBlocked = false, Name = "John", RegistrationDate = DateTime.Now },
-				new User {Id = 3, Age = 40, Bio = "I'm engineer", IsBlocked = false, Name = "Ivan", RegistrationDate = DateTime.Now },
-				new User {Id = 4, Age = 50, Bio = "I'm cook", IsBlocked = false, Name = "Anton", RegistrationDate = DateTime.Now },
-				new User {Id = 5, Age = 60, Bio = "I'm cat", IsBlocked = false, Name = "Alex", RegistrationDate = DateTime.Now },
-				new User {Id = 6, Age = 70, Bio = "I'm oldman", IsBlocked = false, Name = "Sasha", RegistrationDate = DateTime.Now },
-			};
-
-
-
 			_logger = logger;
 		}
 
 		public IActionResult Index()
 		{
-			return View(users);
+			return Redirect("~/Movie");
 		}
 
 		public IActionResult Privacy()
