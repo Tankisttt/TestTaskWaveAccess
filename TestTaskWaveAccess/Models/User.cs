ce78b2d4d4e7527dd53666069f9cf7357f00b37a
Роман Бачурин <romanbachurin@gmail.com>
2021-02-19 11:40:07 +0500

    Created example things

--- TestTaskWaveAccess/Models/User.cs
+++ TestTaskWaveAccess/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,8 @@ namespace TestTaskWaveAccess.Models
 	{
 		public int Id { get; set; }
 		public int Age { get; set; }
-		public int IsBlocked { get; set; }
+		public bool IsBlocked { get; set; }
+		[Required]
 		public string Name { get; set; }
 		public DateTime RegistrationDate { get; set; }
 		/// <summary>
